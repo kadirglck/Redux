9363c19fc70dd819f723dd437c5aba42f06d4a72
kadir gölcük <kadirglk12@gmail.com>
2020-01-04 00:47:40 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
-        <View/>
+        <Provider store={createStore(reducers)}>
+            <View>
+            <Header headerText="Redux"/>
+            <LibraryList/>
+            </View>
+        </Provider>
     );
 };
 
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import LibraryReducer from './LibraryReducer';
+
+
+export default combineReducers({
+    libraries: LibraryReducer
+});
+
